7c8543dbc4444bc30a837775df08aefbe28e392a
hmziqrs <hmziqrs@gmail.com>
2024-10-09 19:57:41 +0500

    added logging

--- src/router.rs
+++ src/router.rs
@@ -1,21 +1,25 @@
-use axum::{
-    response::Json, routing, Router
-};
-use serde_json::{Value, json};
-
+use axum::{response::Json, routing, Router};
+use serde_json::{json, Value};
+use tower_http::trace::TraceLayer;
+use tracing;
 
 use crate::modules;
 
 pub fn router() -> Router {
-    let auth_v1_routes: Router = Router::new()
-        .route("/log_in", routing::post(modules::auth_v1::controller::login));
+    tracing_subscriber::fmt()
+        .with_max_level(tracing::Level::DEBUG)
+        .init();
+
+    let auth_v1_routes: Router = Router::new().route(
+        "/log_in",
+        routing::post(modules::auth_v1::controller::login),
+    );
 
     return Router::new()
         .route("/", routing::get(handler))
-        .nest("/auth/v1", auth_v1_routes);
-        
+        .nest("/auth/v1", auth_v1_routes)
+        .layer(TraceLayer::new_for_http());
 }
-
 
 async fn handler() -> Json<Value> {
     Json(json!({

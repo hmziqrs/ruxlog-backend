92a2b6879e23b3f230a8ee497224e4f48d254f3f
hmziqrs <hmziqrs@gmail.com>
2024-10-15 20:25:05 +0500

    migrations

---
+++ src/db/migrations/2024-10-15-144958_email_verification/up.sql
@@ -0,0 +1,14 @@
+-- Your SQL goes here
+DROP TABLE IF EXISTS "migrations";
+ALTER TABLE "users" ADD COLUMN "is_verified" BOOL NOT NULL;
+
+CREATE TABLE "email_verifications"(
+	"id" INT4 NOT NULL PRIMARY KEY,
+	"user_id" INT4 NOT NULL,
+	"code" VARCHAR NOT NULL,
+	"expires_at" TIMESTAMP NOT NULL,
+	"created_at" TIMESTAMP NOT NULL,
+	"updated_at" TIMESTAMP NOT NULL,
+	FOREIGN KEY ("user_id") REFERENCES "users"("id")
+	UNIQUE ("user_id", "code")
+);

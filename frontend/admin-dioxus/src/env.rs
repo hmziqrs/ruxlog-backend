a1a7279cd9d4dd535f162be278356e47cffcaf96
hmziqrs <hmziqrs@gmail.com>
2025-11-21 12:54:16 +0500

    refactor: simplify environment variable retrieval in env.rs files

--- frontend/admin-dioxus/src/env.rs
+++ frontend/admin-dioxus/src/env.rs
@@ -1,11 +1,2 @@
-pub const APP_API_URL: &str = match std::option_env!("SITE_URL") {
-    Some(val) => val,
-    None => "http://localhost:9999",
-};
-pub const APP_CSRF_TOKEN: &str = match std::option_env!("CSRF_KEY") {
-    Some(val) => val,
-    None => "dWx0cmEtaW5zdGluY3QtZ29rdQ==",
-};
-
-
-
+pub const APP_API_URL: &str = std::env!("SITE_URL");
+pub const APP_CSRF_TOKEN: &str = std::env!("CSRF_KEY");

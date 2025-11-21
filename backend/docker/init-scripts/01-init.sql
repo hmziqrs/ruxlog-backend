57a426a817d8028f41ffa05f18050434b45e080a
hmziqrs <hmziqrs@gmail.com>
2025-11-21 15:25:48 +0500

    fix(init_sql): update user insertion to include is_verified field and secure passwords

--- backend/docker/init-scripts/01-init.sql
+++ backend/docker/init-scripts/01-init.sql
@@ -9,12 +9,12 @@ BEGIN
         RETURN;
     END IF;
 
-    INSERT INTO users (name, email, password, role)
-    SELECT 'Super Admin', 'superadmin@blog.hmziq.rs', 'password123', 'super-admin'
+    INSERT INTO users (name, email, password, role, is_verified)
+    SELECT 'Super Admin', 'superadmin@blog.hmziq.rs', '$argon2id$v=19$m=19456,t=2,p=1$lSCKYb1/M4+3K6ISk0yThw$qHCi4SuMio5DkVgZFHOjVglYv/PiIOdm08fQIMMP5w4', 'super-admin', true
     WHERE NOT EXISTS (SELECT 1 FROM users WHERE email = 'superadmin@blog.hmziq.rs');
 
-    INSERT INTO users (name, email, password, role)
-    SELECT 'Admin User', 'admin@blog.hmziq.rs', 'password123', 'admin'
+    INSERT INTO users (name, email, password, role, is_verified)
+    SELECT 'Admin User', 'admin@blog.hmziq.rs', '$argon2id$v=19$m=19456,t=2,p=1$cUZvIEd5Elez2IPBLALqiw$lZ40B9v+Vg5zm0eyY4VkqPGBXOi6l1/9n+dN0Fv/hug', 'admin', true
     WHERE NOT EXISTS (SELECT 1 FROM users WHERE email = 'admin@blog.hmziq.rs');
 END;
 $$;
